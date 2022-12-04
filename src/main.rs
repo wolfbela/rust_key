8a0643c78f883dc9ed582dc6617dbbf0df07fc86
Elie Ubogi <elie.ubogi@gmail.com>
2022-12-04 18:21:48 -0500

    update basis

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 pub mod app;
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html!(
+        <div>
+            <p>{"Hello World"}</p>
+        </div>
+    )
+}
 
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
