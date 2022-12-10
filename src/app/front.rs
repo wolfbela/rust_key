59df88dd3b4ca146087b45b9c0884dc8e3b1ae1a
wolfbela <zeeliewolfbela@gmail.com>
2022-12-10 23:28:53 +0100

    maybe final refacto

--- src/app/front.rs
+++ src/app/front.rs
@@ -1,36 +1,53 @@
-pub mod login;
-pub mod tabs;
+pub mod main_page;
+pub mod master_loggin_page;
 
+use crate::app::back::login_gestion::password_storing::Password;
 use iced::Sandbox;
-use tabs::Tabs;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct PasswordManager {
-    tabs: Tabs,
+    master_login_page: Tab,
+    main_page: Tab,
+    is_logged: bool,
+}
+
+#[derive(Debug)]
+enum Tab {
+    StartPage { master_password: String },
+    MainPage { passwords: Vec<Password> },
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    MasterPasswordInputChange(String),
-    SubmitMasterPassword,
-    LoginNameChange(String),
-    LoginPasswordChange(String),
-    AddLogin,
+    MasterPasswordChange(String),
+    SubmitMasterPasswordPress,
+    AddLoginPress,
     RemoveLogin,
+    LoginChange(String),
+    PasswordChange(String),
 }
 
 impl Sandbox for PasswordManager {
     type Message = Message;
 
     fn new() -> Self {
-        Self::default()
+        Self::new()
     }
 
     fn title(&self) -> String {
         String::from("RustKey")
     }
 
-    fn update(&mut self, message: Self::Message) {}
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::MasterPasswordChange(_) => todo!(),
+            Message::SubmitMasterPasswordPress => todo!(),
+            Message::AddLoginPress => todo!(),
+            Message::RemoveLogin => todo!(),
+            Message::LoginChange(_) => todo!(),
+            Message::PasswordChange(_) => todo!(),
+        }
+    }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
         todo!()
