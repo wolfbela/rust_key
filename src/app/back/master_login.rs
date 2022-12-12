ad3f0a2da7756dcd470ccade5f446c1aac3f5cf1
wolfbela <zeeliewolfbela@gmail.com>
2022-12-12 14:40:02 +0100

    store master password in file (NOT FINISHED)

--- src/app/back/master_login.rs
+++ src/app/back/master_login.rs
@@ -1,3 +1,4 @@
+use rand::{rngs::StdRng, Rng, SeedableRng};
 use ring::{digest, pbkdf2};
 use serde::{Deserialize, Serialize};
 use std::{fs, num::NonZeroU32};
@@ -12,7 +13,40 @@ enum Error {
 #[derive(Deserialize, Serialize)]
 struct MasterPassword {
     hashed_password: String,
-    salt: [u8; 16],
+    salt: vec![u8; 32],
+}
+
+/*
+This function will serialize the MasterPassword struct into a json.
+After that, the json will be store in the computer.
+*/
+#[allow(dead_code)]
+pub fn register_master_password(new_master_password: &String) {
+    let mut struct_master_password = MasterPassword {
+        hashed_password: String::from(""),
+        salt: [0; 32],
+    };
+
+    /*
+    Creation of the salt.
+    The goal of the salt is to counter the rainbow table with a random string associate with the master password.
+    To do so, it's generate a new random generator with StdRng::from_entropy().
+    After that, it use the rng to fill the salt variable.
+    */
+    let mut rng = StdRng::from_entropy();
+    rng.fill(&mut struct_master_password.salt);
+
+    /*
+    This part of programme will concatenaite the master password with a salt.
+    */
+    let mut unhashe_string = new_master_password.clone();
+    unhashe_string.push_str(
+        String::from_utf8(struct_master_password.salt.to_vec())
+            .unwrap()
+            .as_str(),
+    );
+
+    let serialized_struct = serde_json::to_string(&struct_master_password).unwrap();
 }
 
 /*
