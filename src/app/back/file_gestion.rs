11f76a552d94231ccdcfe2abd13895d5bc36c0cf
wolfbela <zeeliewolfbela@gmail.com>
2022-12-31 15:29:28 +0100

    file encryption: finish encryption (NOT TESTED YET)

--- src/app/back/file_gestion.rs
+++ src/app/back/file_gestion.rs
@@ -1,13 +1,47 @@
+use std::vec;
+
+use rand::{rngs::StdRng, Rng, SeedableRng};
 use ring::aead::*;
 
-struct NonceSequenceTest {
-    nonces: Vec<u8>,
+pub mod nonce_sequence_gestion;
+
+struct MyNonceSequence {
+    nonces: Vec<Nonce>,
 }
 
-impl NonceSequence for NonceSequenceTest {
+impl NonceSequence for MyNonceSequence {
     fn advance(&mut self) -> Result<Nonce, ring::error::Unspecified> {
-        todo!()
+        match self.nonces.pop() {
+            Some(nonce) => Ok(nonce),
+            None => Err(ring::error::Unspecified),
+        }
     }
+}
+
+/*
+Each block has to be of 64 bits. each block need a nonce to have unique encryption. A nonce (Number once = number used one time).
+Here, the goal is to generate a random nonce for each block and put all of those into res.
+*/
+#[allow(dead_code)]
+fn create_nonce_sequence(plane_text: &[u8]) -> Vec<Nonce> {
+    let nb_of_blocks = plane_text.len() / 64 + 1;
+
+    let mut res: Vec<Nonce> = Vec::new();
+
+    /*
+    The, tmp_nonce_arr is an temporary variable used to create one nonce.
+    */
+    let mut tmp_nonce_arr: [u8; 12] = [0x0; 12];
+    for _ in 0..nb_of_blocks {
+        /*
+        We fill the temporary variable with random number before puting it in the nonce vector.
+        */
+        let mut rng = StdRng::from_entropy();
+        rng.fill(&mut tmp_nonce_arr);
+        res.push(Nonce::assume_unique_for_key(tmp_nonce_arr));
+    }
+
+    res
 }
 
 /*
@@ -18,24 +52,44 @@ the goal of the algorithme is to take the whole block, multiplies is by the 'enc
 Also, each block is associated with a counter wich ensure an encryption wich is unique.
 */
 #[allow(dead_code)]
-pub fn encrypt_content(content: &str, key: &[u8], nonces: Nonce) -> String {
+pub fn encrypt_content(content: &str, key: &[u8]) -> String {
     /*
-    to start, will create the encryption_key from the general key gave by the hashing of the master password
+    Instantiation of nonce sequence, it is necessary to get the encryption key
     */
-    let _encryption_key = SealingKey::new(
+    let nonce_sequence = MyNonceSequence {
+        nonces: create_nonce_sequence(content.as_bytes()),
+    };
+
+    /*
+    To start, will create the encryption_key from the general key gave by the hashing of the master password
+    */
+    let mut encryption_key = SealingKey::new(
         UnboundKey::new(&CHACHA20_POLY1305, &key).unwrap(),
-        NonceSequenceTest { nonces: vec![] },
+        nonce_sequence,
     );
-    let mut vec_content = content.as_bytes().to_vec();
 
     /*
     Here, we create the necessary space for the concatenation of the tag with the block
     */
+    let mut vec_content = content.as_bytes().to_vec();
     for _ in 0..CHACHA20_POLY1305.tag_len() {
         vec_content.push(0);
     }
 
-    todo!()
+    let additional_data: [u8; 0] = [];
+
+    /*
+    Encryption of the file
+    The in_out variable has the file contente in clear.
+    The funtion will encrypte the file content and put it into the vec_content var again.
+    */
+    SealingKey::seal_in_place_append_tag(
+        &mut encryption_key,
+        Aad::from(additional_data),
+        &mut vec_content,
+    );
+
+    String::from_utf8(vec_content).unwrap()
 }
 
 #[allow(dead_code)]
